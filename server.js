eb470589833db76504ca4f874c8e0deb706df21b
apat30 <aditi3049@gmail.com>
2018-02-20 21:48:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/Article-one', function(req, res){
 });
 
 app.get('/Article-Two', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'Article-Two.html'))
+   res.sendFile(path.join(__dirname, 'ui', 'Article- two.html'))
 });
 
 app.get('/Article-Three', function(req, res){
