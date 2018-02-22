eea9cf263fe7c3f0833749c2296aa4b1ec05737d
apat30 <aditi3049@gmail.com>
2018-02-22 21:36:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element= document.getElementById('main-text');
 
 element.innerHTML= 'New value';
 
-var img= document.getElementById('madi');
+var img= document.getElementById('img');
 img.onclick=function (){
     img.style.marginLeft= '100px';
 };
