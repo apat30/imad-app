21544dc99e9a996eb000021af0ff8ef9b00a5be3
apat30 <aditi3049@gmail.com>
2018-03-07 23:42:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config= {
     user: 'aditi3049',
     database: 'aditi3049',
     host: 'http://db.imad.hasura-app.io',
-    port:'5432',
+    port:'80',
     password: process.envDB_PASSWORD
 }
 var app = express();
