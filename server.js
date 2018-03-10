c91b201bb5ef76b3be2610fd5e8fc5671c86d3fa
apat30 <aditi3049@gmail.com>
2018-03-10 18:41:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,12 +128,49 @@ app.post('/create-user', function(req, res)
                 }
             else
                 {
-                    res.send('User successfully created: ' +username);
+                     res.send('User successfully created: ' +username);
                 }
     });
     
 });
 
+app.post('/login', function(req, res)
+{
+     //Username and password
+    var username= req.body.username;
+    var password= req.body.password;
+   
+    //Saving into database
+    pool.query('SELECT * FROM  "User" username=$1',[username], function(err, result)
+    {
+         if(err)
+                {
+                    res.status(500).send(err.toString());
+                }
+            else
+                {
+                    if(result.rows.length === 0)
+                    {
+                        res.send(403).send('Username/password is invalid');
+                    }
+                    else
+                    {
+                        var dbString= result.rows[0].password;
+                        var salt= dbString.split('$')[2];
+                        var hashedPassword= hash(password, salt);
+                            if(hashedPassword=== dbString)
+                            {
+                                res.send('cREDENTIALS are correct!!');
+                            }
+                            else
+                            {
+                                res.send(403).send('Username/password is invalid');
+                            }
+                    }
+                }
+    
+    });
+});
 var pool= new pool(config);
 app.get('/Test-db', function(req, res)
 {
