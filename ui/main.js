b9fcd39f74bd9140b780ef4971e11a9089d7ffa1
apat30 <aditi3049@gmail.com>
2018-03-12 22:29:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,20 +8,20 @@ submit.onclick=function()
     //Capture the response and store it in a variable
     request.onreadystatechange= function()
     {
-        if(request.readystate===XMLHttpRequest.DONE)
+        if(request.readyState===XMLHttpRequest.DONE)
         {
             //Take some action
-            if(request.state===200)
+            if(request.status===200)
             {
                 //Capture a list of names and render a list of it
              console.log('User logged in');
              alert('Logged in successful');
              }
-             else if(request.state=== 403)
+             else if(request.status=== 403)
              {
                  alert('Username/ password invalid');
              }
-             else if(request.state=== 500)
+             else if(request.status=== 500)
              {
                  alert('Something went wrong on the server');
              }
