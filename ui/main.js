66fcd68716a03d259d3139a85ed3238e29059397
apat30 <aditi3049@gmail.com>
2018-03-12 22:01:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,40 @@
-console.log('Loaded!');
+//Submit username and login
+var submit= document.getElementById('submit_btn');
+submit.onclick=function()
+{
+    //Create a request object
+    var request= new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange= function()
+    {
+        if(request.readystate===XMLHttpRequest.DONE)
+        {
+            //Take some action
+            if(request.state===200)
+            {
+                //Capture a list of names and render a list of it
+             console.log('User logged in');
+             alert('Logged in successful');
+             }
+             else if(request.state=== 403)
+             {
+                 alert('Username/ password invalid');
+             }
+             else if(request.state=== 500)
+             {
+                 alert('Something went wrong on the server');
+             }
+        }
+        //Not yet done
+    };
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
 
-//Change the text of the main-text file
-var element= document.getElementById('main-text');
+    request.open('POST', 'http://aditi3049.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    reques.send(JSON.stringify({username: username, password:password}));
+};
 
-/*element.innerHTML= 'New value';
-
-var img= document.getElementById('madi');
-    img.onclick=function(){
-        img.style.marginLeft= '100px';
-};*/
